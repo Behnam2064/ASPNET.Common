b52af54b12e65727a537a9526bb70fdd574a5419
administrator <172.19.0.5\Doctors$>
2024-01-21 11:14:16 +0330

    Some filter added

---
+++ ASPNET.Common/AuthorizeAttributeFilters/AllowRemoteAuthorizeAttribute.cs
@@ -0,0 +1,63 @@
+﻿using ASPNET.Common.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ASPNET.Common.AuthorizeAttributeFilters
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false)]
+    public class AllowRemoteAuthorizeAttribute : Attribute, IAuthorizationFilter
+    {
+        
+
+        public bool AllowRemote { get; set; }
+        public string RouteController { get; set; }
+        public string RouteAction { get; set; }
+
+        
+        public AllowRemoteAuthorizeAttribute(bool allowRemote, string RouteController, string RouteAction)
+        {
+
+            if (RouteController == null)
+                throw new ArgumentNullException(nameof(RouteController));
+
+            if (RouteAction == null)
+                throw new ArgumentNullException(nameof(RouteAction));
+
+
+            this.AllowRemote = allowRemote;
+            this.RouteController = RouteController;
+            this.RouteAction = RouteAction;
+
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            //throw new NotImplementedException();
+            if (!AllowRemote && !context.HttpContext.Request.IsLocal())
+            {
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.Unauthorized; // = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+                //throw new UnauthorizedAccessException();
+                context.Result = 
+                new RedirectToRouteResult(new RouteValueDictionary
+                {
+                    { "controller", RouteController },   // Change "Account" to your login controller name
+                    { "action", RouteAction }       // Change "Login" to your login action method name
+                });
+
+                //context.HttpContext.Response.Redirect(string.Empty);
+            }
+        }
+
+
+
+    }
+}
---
+++ ASPNET.Common/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPNET.Common.Extensions
+{
+    public static class HttpRequestExtensions
+    {
+        public static bool IsLocal(this HttpRequest req)
+        {
+            var connection = req.HttpContext.Connection;
+            if (connection.RemoteIpAddress != null)
+            {
+                if (connection.LocalIpAddress != null)
+                {
+                    return connection.RemoteIpAddress.Equals(connection.LocalIpAddress);
+                }
+                else
+                {
+                    return IPAddress.IsLoopback(connection.RemoteIpAddress);
+                }
+            }
+
+            // for in memory TestServer or when dealing with default connection info  
+            if (connection.RemoteIpAddress == null && connection.LocalIpAddress == null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}

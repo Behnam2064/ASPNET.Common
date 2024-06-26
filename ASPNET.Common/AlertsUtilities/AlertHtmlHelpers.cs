0c72d9a52909bfb915f8778894f92b3927c880b0
administrator <172.19.0.5\Doctors$>
2024-06-26 08:55:57 +0330

    The Bootstrap' alert Tag helper has created

---
+++ ASPNET.Common/AlertsUtilities/AlertHtmlHelpers.cs
@@ -0,0 +1,46 @@
+﻿using ASPNET.Common.PaginationUtilities;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPNET.Common.AlertsUtilities
+{
+    public enum AlertBootstrapType
+    {
+        primary,
+        secondary,
+        success,
+        warning,
+        danger,
+        info,
+        light,
+        dark
+    }
+
+    public static class AlertHtmlHelpers
+    {
+        public static IHtmlContent AlertBootstrap(this IHtmlHelper htmlHelper, object Content, AlertBootstrapType alertType)
+        {
+            return new HtmlString($"""
+                <div class="alert alert-{alertType}" role="alert">
+                {Content}
+                </div>
+                """);
+        }
+
+        public static IHtmlContent AlertDismissibleBootstrap(this IHtmlHelper htmlHelper, object Content, AlertBootstrapType alertType)
+        {
+
+            return new HtmlString($"""
+                <div class="alert alert-{alertType} alert-dismissible" role="alert">
+                {Content}
+                  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                """);
+        }
+    }
+}

cb69888b47cde69b15ddebb0d418120e8c696248
Behnam2064 <Behnam@DESKTOP-0PJLIOQ>
2025-01-23 19:15:35 +0330

    feat:The MultipartFormDataAttribute has been added to ASPNET.Common

---
+++ ASPNET.Common/UploadAttributes/MultipartFormDataAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPNET.Common.UploadAttributes
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class MultipartFormDataAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var request = context.HttpContext.Request;
+
+            if (request.HasFormContentType
+                && request.ContentType.StartsWith("multipart/form-data", StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+
+            context.Result = new StatusCodeResult(StatusCodes.Status415UnsupportedMediaType);
+        }
+    }
+}

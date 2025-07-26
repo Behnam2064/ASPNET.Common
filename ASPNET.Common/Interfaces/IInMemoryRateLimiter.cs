ada69569cba0f1363698c5a4ab31101d86193c66
Behnam2064 <Behnam@DESKTOP-0PJLIOQ>
2025-07-26 09:47:16 +0330

    feat:feat:The InMemoryRateLimiter class added to handle request rate

---
+++ ASPNET.Common/Interfaces/IInMemoryRateLimiter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPNET.Common.Interfaces
+{
+    public enum MemoryRateRequestType
+    {
+        AuthWebSocket,
+        Download,
+        ActivationWebSocket,
+        // موارد دلخواه دیگر...
+    }
+
+    public enum RateLimitTimeUnit
+    {
+        Second,
+        Minute,
+        Hour
+    }
+
+    public class RateLimitRule
+    {
+        public int MaxRequests { get; set; }
+        public RateLimitTimeUnit TimeUnit { get; set; }
+        public int Duration { get; set; } // مثلاً 1 دقیقه یا 10 ثانیه
+    }
+    public interface IInMemoryRateLimiter
+    {
+    }
+}
---
+++ ASPNET.Common/RequestRateLimiter/InMemoryRateLimiter.cs
@@ -0,0 +1,52 @@
+﻿using ASPNET.Common.Interfaces;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPNET.Common.RequestRateLimiter
+{
+
+    public class InMemoryRateLimiter : IInMemoryRateLimiter
+    {
+        private readonly Dictionary<MemoryRateRequestType, RateLimitRule> _rules;
+        private readonly ConcurrentDictionary<string, List<DateTime>> _requestLogs = new();
+
+        public InMemoryRateLimiter(Dictionary<MemoryRateRequestType, RateLimitRule> rules)
+        {
+            _rules = rules;
+        }
+
+        public bool IsRequestAllowed(MemoryRateRequestType type, string clientId)
+        {
+            if (!_rules.TryGetValue(type, out var rule))
+                return true; 
+
+            string key = $"{type}_{clientId}";
+            var now = DateTime.UtcNow;
+            var windowStart = rule.TimeUnit switch
+            {
+                RateLimitTimeUnit.Second => now.AddSeconds(-rule.Duration),
+                RateLimitTimeUnit.Minute => now.AddMinutes(-rule.Duration),
+                RateLimitTimeUnit.Hour => now.AddHours(-rule.Duration),
+                _ => now
+            };
+
+            var log = _requestLogs.GetOrAdd(key, _ => new List<DateTime>());
+
+            lock (log)
+            {
+                log.RemoveAll(t => t < windowStart);
+
+                if (log.Count >= rule.MaxRequests)
+                    return false;
+
+                log.Add(now);
+                return true;
+            }
+        }
+    }
+
+}
